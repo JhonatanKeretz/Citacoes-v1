3c62c5afb9df608f090b75c73e987a7fc88dc07e
Jhonatan Keretz <92455620+JhonatanKeretz@users.noreply.github.com>
2022-03-31 16:33:04 -0300

    Add files via upload

---
+++ src/components/Citacao.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="citacao">
+        <slot name="fonte"></slot>
+        <slot></slot>
+        <div class="autor"><slot name="autor"></slot></div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+
+    h1 {
+        color: red
+    }
+
+    .autor {
+        float: right;
+    }
+</style>
